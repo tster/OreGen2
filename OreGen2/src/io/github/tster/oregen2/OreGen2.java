7c28b6f214cf25262ce94c985aef6988c917287f
Thomas Bush <bushthomas@hotmail.com>
2014-01-05 14:46:28 +0100

    Added code

--- OreGen2/src/io/github/tster/oregen2/OreGen2.java
+++ OreGen2/src/io/github/tster/oregen2/OreGen2.java
@@ -43,10 +43,8 @@ public final class OreGen2 extends JavaPlugin implements Listener {
 		
 		@EventHandler
 	    public void cobbleGeneration(BlockFromToEvent event) {
-			getLogger().info("cobble generation" + event.getToBlock().getType() + event.getToBlock().getRelative(event.getFace()).getType());
 			if (event.getToBlock().getType() == Material.COBBLESTONE && event.getToBlock().getRelative(event.getFace()).getType() == Material.STATIONARY_WATER) {
 	        	// If the 'to' block is cobble now and the block behind that is water
-	        	getLogger().info("called");	
 	        	event.getToBlock().setType(chooseBlock());
 	        	event.setCancelled(true);
 	        	// Set the block to a random block - chosen by the chooseBlock function
@@ -63,35 +61,28 @@ public final class OreGen2 extends JavaPlugin implements Listener {
 			    	// If the max number of decimal places is bigger than maxDecimalPlaces (starts at 0)
 			    	maxDecimalPlaces = value.toString().length() - value.toString().indexOf('.') - 1;
 			    	// Set the max number of decimal places
-			    	getLogger().info("1  "+ maxDecimalPlaces);
 			    }
 			}
 			// Long method to find the highest number of decimal places in the config file
 			// This value will be used to convert decimal chances into draws
 			for (Object value : configMap.values()) {
-				getLogger().info("test" + value.toString());
 				totalDraws = totalDraws + Double.parseDouble(value.toString()) * Math.pow(10,maxDecimalPlaces);
 			}
 			// Finds the total number of draws - in doing so converting decimal chances to draws
-			getLogger().info("" + totalDraws);
 			drawNumber = rand.nextInt((int) totalDraws) + 1;
-			getLogger().info("draws" + drawNumber);
 			// Generates a random number between 1 and the total number of draws
 			for (Map.Entry<String, Object> entry : configMap.entrySet()) {
 				// For each key in config
 				if (drawNumber <= (Double.parseDouble(entry.getValue().toString()) * Math.pow(10, maxDecimalPlaces)) + currentDraw) {
 					// If the random number is less than or equal to the key's value + the current draw
-					getLogger().info("caught" + entry.getKey());
 					if (blockMaterial == null) {
 						blockMaterial = Material.getMaterial(entry.getKey());
-						getLogger().info("should out: " + blockMaterial);
 					}
 
 					// Set the output block to the material associated with the key name
 				} else {
 					currentDraw = (int) (currentDraw + (Double.parseDouble(entry.getValue().toString()) * Math.pow(10, maxDecimalPlaces)));
 					// Otherwise add to the current draw
-					getLogger().info("added");
 				}
 			}
 			return blockMaterial;
